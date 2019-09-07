94adad80ab958c2c4912628aba8b63f2a297374d
hayden <haydenwb@gmail.com>
2019-09-07 19:14:10 +0100

    changed to div, about to add delete all

--- js/app.js
+++ js/app.js
@@ -15,23 +15,23 @@ const handleNewItemFormSubmit = function (event) {
 }
 
 const createSubmissionItem = function (form) {
-  const newItem = document.createElement('li');
+  const newItem = document.createElement('div');
   newItem.classList.add('records-item');
 
   const writingSubmitted = document.createElement('p');
-  writingSubmitted.textContent = `Title of Work Submitted: ${form['writing-submitted'].value}`;
+  writingSubmitted.textContent = `Title: ${form['writing-submitted'].value}`;
   newItem.appendChild(writingSubmitted);
 
   const typeOfWriting = document.createElement('p');
-  typeOfWriting.textContent = `Type of Writing Submitted: ${form['type-of-writing'].value}`;
+  typeOfWriting.textContent = `Type: ${form['type-of-writing'].value}`;
   newItem.appendChild(typeOfWriting);
 
   const placeSubmittedTo = document.createElement('p');
-  placeSubmittedTo.textContent = `Place Submitted to: ${form['place-submitted-to'].value}`;
+  placeSubmittedTo.textContent = `Submitted to: ${form['place-submitted-to'].value}`;
   newItem.appendChild(placeSubmittedTo);
 
   const typeOfPlace = document.createElement('p');
-  typeOfPlace.textContent = `Type of Place Submitted to: ${form['type-of-place'].value}`;
+  typeOfPlace.textContent = `Type: ${form['type-of-place'].value}`;
   newItem.appendChild(typeOfPlace);
 
   return newItem;
