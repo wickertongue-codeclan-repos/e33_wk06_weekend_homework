ef838a1160b65dca8d58118f1f26552c84b3dd98
hayden <haydenwb@gmail.com>
2019-09-07 17:13:43 +0100

    reluctantly moved away from table idea

--- js/app.js
+++ js/app.js
@@ -9,41 +9,45 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 const handleWritingSubmit = function (event) {
+  const writingSubmitted = event.target['writing-submitted'].value; 
   event.preventDefault();
 
-  const existingParent = document.querySelector('#records');
-  const newTableData = document.createElement('td');
+  const existingParent = document.querySelector(`#records`);
+  const newTableData = document.createElement('p');
   existingParent.appendChild(newTableData);
 
-  newTableData.textContent = `${event.target['writing-submitted'].value}`
+  newTableData.textContent = `Title of piece: ${writingSubmitted}`
 };
 
 const handleTypeWritingSubmit = function (event) {
+  const typeOfWriting = event.target['type-of-writing'].value
   event.preventDefault();
 
-  const existingParent = document.querySelector('#records');
-  const newTableData = document.createElement('td');
+  const existingParent = document.querySelector(`#records`);
+  const newTableData = document.createElement('p');
   existingParent.appendChild(newTableData);
 
-  newTableData.textContent = `${event.target['type-of-writing'].value}`
+  newTableData.textContent = `Type of writing: ${typeOfWriting}`
 };
 
 const handlePlaceSubmittedToSubmit = function (event) {
+  const placeSubmittedTo = event.target['place-submitted-to'].value  
   event.preventDefault();
 
-  const existingParent = document.querySelector('#records');
-  const newTableData = document.createElement('td');
+  const existingParent = document.querySelector(`#records`);
+  const newTableData = document.createElement('p');
   existingParent.appendChild(newTableData);
 
-  newTableData.textContent = `${event.target['place-submitted-to'].value}`
+  newTableData.textContent = `Submitted to: ${placeSubmittedTo}`
 };
 
 const handleTypeOfPlaceSubmit = function (event) {
+  const typeOfPlace = event.target['type-of-place'].value
   event.preventDefault();
 
-  const existingParent = document.querySelector('#records');
-  const newTableData = document.createElement('td');
+  const existingParent = document.querySelector(`#records`);
+  const newTableData = document.createElement('p');
   existingParent.appendChild(newTableData);
 
-  newTableData.textContent = `${event.target['type-of-place'].value}`
+  newTableData.textContent = `Type of location: ${typeOfPlace}`
 };
